fea692da9981f25afda08e6d53cd3d9d9eab27ab
mg-degroot <mg.degroot@avans.nl>
2025-02-12 14:47:32 +0100

    Simple hasher to generate Identity compatible password hashes

---
+++ PasswordHasher/Program.cs
@@ -0,0 +1,21 @@
+﻿using PasswordHasher;
+string? plainText = string.Empty;
+if (args.Length != 1)
+{
+    Console.Write("password: ");
+    while (true)
+    {
+        var key = Console.ReadKey(true);
+        if (key.Key == ConsoleKey.Enter)
+            break;
+        plainText += key.KeyChar;
+    }
+}
+else
+{
+    plainText = args[0];
+}
+var hashed = PasswordHelper.HashPassword(plainText);
+Console.WriteLine(hashed);
+
+
